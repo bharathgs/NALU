e0d852419531779635342d3f7e7e19671c6e7349
bharath g s <royalkingpin@gmail.com>
2018-08-06 19:17:39 +0530

    minor updates

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('nalu/__init__.py', 'r') as f:
 with open('README.md', 'r', encoding='utf-8') as f:
     readme = f.read()
 
-REQUIRES = []
+REQUIRES = ['torch', 'numpy']
 
 setup(
     name='NALU',
